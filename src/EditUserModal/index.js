34ae3d1ea03c42d3f18e4396cce68ecbe3c09cbf
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-05-15 13:26:11 -0500

    ok the edit modal is taking the clicks, but the radios arent showing as clicked, and theres a cors issue

--- src/EditUserModal/index.js
+++ src/EditUserModal/index.js
@@ -5,6 +5,7 @@ import '../index.css'
 export default function EditUserModal({ userToEdit, updateUser, closeModal }) {
   const [userPrefs, setUserPrefs] = useState(userToEdit)
   console.log(userPrefs, "******EDIT USER MODAL USERPREFS");
+  console.log(userPrefs[0].big_pref, "big pref with brackets");
   const handleChange = event => setUserPrefs({ ...userPrefs, [event.target.name]: event.target.value })
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -19,19 +20,112 @@ export default function EditUserModal({ userToEdit, updateUser, closeModal }) {
         <h3>Edit Preferences</h3>
       </Header>
       <Modal.Content>
-        <Form onSubmit={handleSubmit}>
+        <h3>{userPrefs[0].name}'s Preferences</h3>
+        <Form onSubmit={handleSubmit} className="this-form">
           <div className="two-columns">
           <Label>How important is a clean dog park to you?:</Label>
           <Form.Input 
             type="radio"
             name="clean_pref"
+            label='Very'
             value='3'
-            checked={userPrefs.clean_pref === '3'}
+            checked={userPrefs[0].clean_pref === '3'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="clean_pref"
+            label='Kinda'
+            value='2'
+            checked={userPrefs[0].clean_pref === '2'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="clean_pref"
+            label='Meh...'
+            value='1'
+            checked={userPrefs[0].clean_pref === '1'}
+            onChange={handleChange}
+          />
+          <Label>How important is a big dog park to you?:</Label>
+          <Form.Input 
+            type="radio"
+            name="big_pref"
+            label='Very'
+            value='3'
+            checked={userPrefs[0].big_pref === '3'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="big_pref"
+            label='Kinda'
+            value='2'
+            checked={userPrefs[0].big_pref === '2'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="big_pref"
+            label='Meh...'
+            value='1'
+            checked={userPrefs[0].big_pref === '1'}
+            onChange={handleChange}
+          />
+          <Label>How important is a fenced-in dog park to you?:</Label>
+          <Form.Input 
+            type="radio"
+            name="fenced_pref"
+            label='Very'
+            value='3'
+            checked={userPrefs[0].fenced_pref === '3'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="fenced_pref"
+            label='Kinda'
+            value='2'
+            checked={userPrefs[0].fenced_pref === '2'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="fenced_pref"
+            label='Meh...'
+            value='1'
+            checked={userPrefs[0].fenced_pref === '1'}
+            onChange={handleChange}
+          />
+          <Label>How important is a busy dog park to you?:</Label>
+          <Form.Input 
+            type="radio"
+            name="busy_pref"
+            label='Very'
+            value='3'
+            checked={userPrefs[0].busy_pref === '3'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="busy_pref"
+            label='Kinda'
+            value='2'
+            checked={userPrefs[0].busy_pref === '2'}
+            onChange={handleChange}
+          />
+          <Form.Input 
+            type="radio"
+            name="busy_pref"
+            label='Meh...'
+            value='1'
+            checked={userPrefs[0].busy_pref === '1'}
             onChange={handleChange}
           />
           </div>
           <Modal.Actions>
-            <Button type="Submit">Update Dog</Button>
+            <Button type="Submit">Update Preferences</Button>
           </Modal.Actions>
         </Form>
       </Modal.Content>
